25396eb4bc071209f3c33ec38d9d41fdeb8cffdc
AaronCur <c00202505@itcarlow.ie>
2018-10-04 10:51:23 +0100

    started lab

---
+++ FSM(Animation)/FSM(Animation)/Climbing.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "State.h"
+#include "Idle.h"
+
+class Climbing : public State
+{
+public:
+	Climbing() {};
+	~Climbing() {};
+	void idle(Animation* a)
+	{
+		std::cout << "Going from Climbing to Idling" << std::endl;
+		a->setCurrent(new Idle());
+	}
+};
---
+++ FSM(Animation)/FSM(Animation)/Jumping.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "State.h"
+#include "Idle.h"
+
+class Jumping: public State
+{
+public:
+	Jumping() {};
+	~Jumping() {};
+	void idle(Animation* a)
+	{
+		std::cout << "Going from Jumping to Idling" << std::endl;
+		a->setCurrent(new Idle());
+	}
+};
