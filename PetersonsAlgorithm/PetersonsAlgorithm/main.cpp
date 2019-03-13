6dbc67938df762ae7bf5508636396494d372bd6d
AaronCur <c00202505@itcarlow.ie>
2019-03-13 15:54:48 +0000

    setup

---
+++ PetersonsAlgorithm/PetersonsAlgorithm/main.cpp
@@ -0,0 +1,28 @@
+#include <thread>
+#include <iostream>
+#include <queue>
+#include <mutex>
+
+
+bool flag[2] = { false,false };
+int turn = 1;
+
+std::mutex mx;
+std::condition_variable cv;
+
+
+void cs1() {
+	while (true) {
+
+	}
+
+}
+void cs2() {
+	while (true) {
+
+	}
+
+}
+int main() {
+
+}
