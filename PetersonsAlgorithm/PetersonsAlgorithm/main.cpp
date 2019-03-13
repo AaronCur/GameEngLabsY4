9a897057acb75c4f45be6a77fda81487ef457cd9
AaronCur <c00202505@itcarlow.ie>
2019-03-13 16:09:51 +0000

    done 2 processes

--- PetersonsAlgorithm/PetersonsAlgorithm/main.cpp
+++ PetersonsAlgorithm/PetersonsAlgorithm/main.cpp
@@ -12,17 +12,52 @@ std::condition_variable cv;
 
 
 void cs1() {
+
 	while (true) {
+		/*entry*/
+
+		flag[0] = true;
+		turn = 1;
+		/*wait*/
+		while (flag[1] && turn == 1) {
+
+
+		}
+		/*critical selection*/
+		std::cout << "1" << std::endl;
+		flag[0] = false;
+
 
 	}
 
 }
 void cs2() {
 	while (true) {
+		/*entry*/
+
+		flag[1] = true;
+		turn = 0;
+		/*wait*/
+		while (flag[0] && turn == 0) {
+
+
+		}
+		/*critical selection*/
+		std::cout << "2" << std::endl;
+		flag[1] = false;
+
 
 	}
 
 }
 int main() {
+
+	std::thread t1(cs1);
+	std::thread t2(cs2);
+
+	t1.join();
+	t2.join();
+
+	std::cin.get();
 
 }
